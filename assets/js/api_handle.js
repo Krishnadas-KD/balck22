6d7b2b4652856d4e1444dc3c30bfb339c1d7ec60
KD <kkrishnadas393@gmail.com>
2025-04-30 01:23:23 +0530

    update

--- assets/js/api_handle.js
+++ assets/js/api_handle.js
@@ -1,3 +1,20 @@
+async function loadHomeSection() {
+    try {
+        const response = await fetch('api/home_api.php');
+        const data = await response.json();
+        
+        if (data.error) {
+            console.error('Error loading about data:', data.error);
+            return;
+        }
+        
+        // Update the HTML with the fetched data
+        document.querySelector('#home-header').textContent = data.title;
+        document.querySelector('#home-desc').textContent = data.content;
+    } catch (error) {
+        console.error('Error fetching about data:', error);
+    }
+}
 async function loadAboutSection() {
     try {
         const response = await fetch('api/about.php');
